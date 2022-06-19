50c82658b339829c260c553891ca6cf04d35f9de
gsfernandes81 <gsfernandes81@gmail.com>
2022-06-19 18:47:00 +0100

    Make the code work with the db

--- polarity/main.py
+++ polarity/main.py
@@ -13,19 +13,21 @@
 # You should have received a copy of the GNU Affero General Public License along with
 # mortal-polarity. If not, see <https://www.gnu.org/licenses/>.
 
-import os
-
 import hikari
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import delete, select
+
 from . import cfg
+from .schemas import Commands
+
+db_engine = create_async_engine(cfg.db_url_async)
+db_session = sessionmaker(db_engine, **cfg.db_session_kwargs)
 
 bot = hikari.GatewayBot(token=cfg.main_token)
 
 
 COMMAND_GUILD_ID = cfg.test_env if cfg.test_env else hikari.UNDEFINED
-
-command_dict = {
-    "xur": {"description": "Xur Infographic", "text": "https://kyber3000.com/Xur"}
-}
 
 
 @bot.listen()
@@ -37,38 +39,42 @@ async def register_commands() -> None:
     """Register ping and info commands."""
     application = await bot.rest.fetch_application()
 
-    commands = [
-        bot.rest.slash_command_builder(
-            "add", "Add a link to the bot, only usable by Kyber et al"
-        )
-        .add_option(
-            hikari.CommandOption(
-                type=hikari.OptionType.STRING,
-                name="name",
-                is_required=True,
-                description="Name of the link to add",
-            )
-        )
-        .add_option(
-            hikari.CommandOption(
-                type=hikari.OptionType.STRING,
-                name="description",
-                is_required=True,
-                description="Description of what is in this link and commmand",
-            )
-        )
-        .add_option(
-            hikari.CommandOption(
-                type=hikari.OptionType.STRING,
-                name="link",
-                is_required=True,
-                description="Link to post when this command is used",
-            )
-        )
-    ] + [
-        bot.rest.slash_command_builder(name, command_dict[name]["description"])
-        for name in command_dict
-    ]
+    async with db_session() as session:
+        async with session.begin():
+            command_list = (await session.execute(select(Commands))).fetchall()
+            command_list = [] if command_list is None else command_list
+            commands = [
+                bot.rest.slash_command_builder(
+                    "add", "Add a link to the bot, only usable by Kyber et al"
+                )
+                .add_option(
+                    hikari.CommandOption(
+                        type=hikari.OptionType.STRING,
+                        name="name",
+                        is_required=True,
+                        description="Name of the link to add",
+                    )
+                )
+                .add_option(
+                    hikari.CommandOption(
+                        type=hikari.OptionType.STRING,
+                        name="description",
+                        is_required=True,
+                        description="Description of what is in this link and commmand",
+                    )
+                )
+                .add_option(
+                    hikari.CommandOption(
+                        type=hikari.OptionType.STRING,
+                        name="link",
+                        is_required=True,
+                        description="Link to post when this command is used",
+                    )
+                )
+            ] + [
+                bot.rest.slash_command_builder(command[0].name, command[0].description)
+                for command in command_list
+            ]
 
     await bot.rest.set_application_commands(
         application=application.id,
@@ -84,10 +90,11 @@ async def handle_interactions(event: hikari.InteractionCreateEvent) -> None:
         # only listen to command interactions, no others!
         return
 
+    await event.interaction.create_initial_response(
+        hikari.ResponseType.MESSAGE_CREATE, f"Working..."
+    )
+
     if event.interaction.command_name == "add":
-        await event.interaction.create_initial_response(
-            hikari.ResponseType.MESSAGE_CREATE, f"Working..."
-        )
         options = event.interaction.options
 
         # Sort the options into a dict
@@ -95,29 +102,55 @@ async def handle_interactions(event: hikari.InteractionCreateEvent) -> None:
         for option in options:
             options_dict[option.name] = option.value
 
-        if options_dict["name"] in ["add", "edit", "remove"] + list(
-            command_dict.keys()
-        ):
-            await event.interaction.edit_initial_response(
-                "A command with that name already exists"
-            )
-            return
+        async with db_session() as session:
+            async with session.begin():
+                additional_commands = (
+                    await session.execute(select(Commands))
+                ).fetchall()
+                additional_commands = (
+                    [] if additional_commands is None else additional_commands
+                )
+                additional_commands = [
+                    command[0].name
+                    for command in (await session.execute(select(Commands))).fetchall()
+                ]
+                if (
+                    options_dict["name"]
+                    in ["add", "edit", "remove"] + additional_commands
+                ):
+                    await event.interaction.edit_initial_response(
+                        "A command with that name already exists"
+                    )
+                    return
 
-        command_dict[options_dict["name"]] = {
-            "description": options_dict["description"],
-            "text": options_dict["link"],
-        }
+                command = Commands(
+                    options_dict["name"].lower(),
+                    options_dict["description"],
+                    options_dict["link"],
+                )
+                session.add(command)
 
         await register_commands()
         await event.interaction.edit_initial_response("Command added")
         return
 
-    if event.interaction.command_name in command_dict:
-        await event.interaction.create_initial_response(
-            hikari.ResponseType.MESSAGE_CREATE,
-            command_dict[event.interaction.command_name]["text"],
-        )
-        return
+    async with db_session() as session:
+        async with session.begin():
+            matching_command = (
+                await session.execute(
+                    select(Commands).where(
+                        Commands.name == event.interaction.command_name
+                    )
+                )
+            ).fetchone()
+            if matching_command is None:
+                # Put a warning message here if you don't find a commmand in the db
+                return
+            command_text_response = matching_command[0].text
+            await event.interaction.edit_initial_response(
+                command_text_response,
+            )
+            return
 
 
 bot.run()
