cccec9f3e16e415389c3e705b56fb3905d4abd65
gsfernandes81 <gsfernandes81@gmail.com>
2022-07-09 18:58:31 +0100

    Add delete_commands heroku target
    This deletes all commands from discord's global registry when run

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 clock: python -OO -m polarity.main & python -OO -m polarity.reset_signaller
 release: cd polarity && alembic upgrade head && cd .. && python -m polarity.release
+delete_commands: python -m polarity.delete_commands
---
+++ polarity/delete_commands.py
@@ -0,0 +1,37 @@
+# Copyright © 2019-present gsfernandes81
+
+# This file is part of "mortal-polarity".
+
+# mortal-polarity is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+
+# "mortal-polarity" is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License along with
+# mortal-polarity. If not, see <https://www.gnu.org/licenses/>.
+
+# RUN WITH CAUTION
+# DELETES ALL PUBLISHED COMMANDS FOR cfg.main_token GLOBALLY
+import asyncio
+import hikari
+
+from . import cfg
+
+rest = hikari.RESTApp()
+
+TOKEN = cfg.main_token
+
+
+async def main():
+    async with rest.acquire(cfg.main_token, hikari.TokenType.BOT) as client:
+        application = await client.fetch_application()
+
+        await client.set_application_commands(
+            application.id, (), guild=hikari.UNDEFINED
+        )
+
+
+asyncio.run(main())
