d205afcb7b5a6d6e24eac13fa9983ec199856672
gsfernandes81 <gsfernandes81@gmail.com>
2022-07-17 15:41:02 +0100

    Add commands to change xur gfx and post urls

--- polarity/controller.py
+++ polarity/controller.py
@@ -67,6 +67,20 @@ async def ls_announcements(ctx: lightbulb.Context):
 
 
 @kyber.child
+@lightbulb.command(
+    "xur_announcements",
+    "Xur announcement management",
+    guilds=[
+        cfg.kyber_discord_server_id,
+    ],
+    inherit_checks=True,
+)
+@lightbulb.implements(lightbulb.SlashSubGroup)
+async def xur_announcements():
+    pass
+
+
+@xur_announcements.child
 @lightbulb.option(
     "option",
     "Enable or disable",
@@ -75,14 +89,14 @@ async def ls_announcements(ctx: lightbulb.Context):
     required=True,
 )
 @lightbulb.command(
-    "xur_announcements",
+    "autoposts",
     "Enable or disable all automatic lost sector announcements",
     auto_defer=True,
     # The following NEEDS to be included in all privledged commands
     inherit_checks=True,
 )
 @lightbulb.implements(lightbulb.SlashSubCommand)
-async def xur_announcements(ctx: lightbulb.Context):
+async def xur_autoposts(ctx: lightbulb.Context):
     option = True if ctx.options.option.lower() == "enable" else False
     async with db_session() as session:
         async with session.begin():
@@ -95,6 +109,85 @@ async def xur_announcements(ctx: lightbulb.Context):
     await ctx.respond(
         "Xur announcements {}".format("Enabled" if option else "Disabled")
     )
+
+
+@xur_announcements.child
+@lightbulb.option(
+    "url",
+    "The url to set",
+    type=str,
+    required=False,
+)
+@lightbulb.command(
+    "infographic_url",
+    "Set the Xur infographic url, to check and post",
+    auto_defer=True,
+    inherit_checks=True,
+)
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def xur_gfx_url(ctx: lightbulb.Context):
+    url = ctx.options.url.lower() if ctx.options.url is not None else None
+    async with db_session() as session:
+        async with session.begin():
+            settings: XurPostSettings = await session.get(XurPostSettings, 0)
+            if ctx.options.url is None:
+                await ctx.respond(
+                    (
+                        "The current Xur Infographic url is <{}>\n"
+                        + "The default Xur Infographic url is <{}>"
+                    ).format(
+                        "unset" if settings is None else settings.url,
+                        "https://kyber3000.com/Xur",
+                    )
+                )
+                return
+            if settings is None:
+                settings = XurPostSettings(0)
+                settings.url = url
+                await settings.initialise_url_params()
+                session.add(settings)
+            else:
+                settings.url = url
+    await ctx.respond("Xur Infographic url updated to <{}>".format(url))
+
+
+@xur_announcements.child
+@lightbulb.option(
+    "url",
+    "The url to set",
+    type=str,
+    required=False,
+)
+@lightbulb.command(
+    "post_url",
+    "Set the Xur infographic url, to check and post",
+    auto_defer=True,
+    inherit_checks=True,
+)
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def xur_post_url(ctx: lightbulb.Context):
+    url = ctx.options.url.lower() if ctx.options.url is not None else None
+    async with db_session() as session:
+        async with session.begin():
+            settings: XurPostSettings = await session.get(XurPostSettings, 0)
+            if ctx.options.url is None:
+                await ctx.respond(
+                    (
+                        "The current Xur Post url is <{}>\n"
+                        + "The default Xur Post url is <{}>"
+                    ).format(
+                        "unset" if settings is None else settings.post_url,
+                        "https://kyber3000.com/Xurpost",
+                    )
+                )
+                return
+            if settings is None:
+                settings = XurPostSettings(0)
+                settings.post_url = url
+                session.add(settings)
+            else:
+                settings.post_url = url
+    await ctx.respond("Xur Post url updated to <{}>".format(url))
 
 
 def register_all(bot: lightbulb.BotApp) -> None:
