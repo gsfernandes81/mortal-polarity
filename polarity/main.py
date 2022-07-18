d56838f5eb1e0bc4af2a2846c5a4530f5ccc33b5
gsfernandes81 <gsfernandes81@gmail.com>
2022-07-18 16:46:11 +0100

    Added no. of guilds to discord status

--- polarity/main.py
+++ polarity/main.py
@@ -16,6 +16,7 @@
 import hikari
 import lightbulb
 import uvloop
+from lightbulb.ext import tasks
 
 from . import cfg, controller, debug_commands, user_commands
 from .autoannounce import arm
@@ -32,9 +33,21 @@ async def on_ready(event: hikari.StartedEvent) -> None:
     await arm(bot)
 
 
+@tasks.task(m=30, auto_start=True, wait_before_execution=False)
+async def autoupdate_status():
+    await bot.wait_for(lightbulb.events.LightbulbStartedEvent, timeout=None)
+    await bot.update_presence(
+        activity=hikari.Activity(
+            name="to {} servers".format(len(bot.cache.get_guilds_view())),
+            type=hikari.ActivityType.LISTENING,
+        )
+    )
+
+
 if __name__ == "__main__":
     user_commands.register_all(bot)
     controller.register_all(bot)
+    tasks.load(bot)
     if cfg.test_env:
         debug_commands.register_all(bot)
     bot.run()
