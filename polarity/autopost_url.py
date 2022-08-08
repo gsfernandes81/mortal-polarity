6ae24950000c0d5c73c617f45e57dc1da5b6c856
gsfernandes81 <gsfernandes81@gmail.com>
2022-08-08 16:16:21 +0100

    Remove UrlAutopostsBase._partials_applied (unused)

--- polarity/autopost_url.py
+++ polarity/autopost_url.py
@@ -219,8 +219,6 @@ class BaseUrlSignal(BaseCustomEvent):
 
 
 class UrlAutopostsBase(AutopostsBase):
-    _partials_applied = False
-
     def __init__(
         self,
         settings_table: Type[UrlPostSettings],
