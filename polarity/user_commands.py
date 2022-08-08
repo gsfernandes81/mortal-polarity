a1584a474488198f7c409eaf0644d076abe64936
gsfernandes81 <gsfernandes81@gmail.com>
2022-08-08 11:02:51 +0100

    Revert "Temp bugfix for invisible control cmds for non-admins"
    This is no longer necessary with the update to hikari-dev110
    
    This reverts commit 5d286afc0b97c5479ec3a36fb334f173ef3d8c18.

--- polarity/user_commands.py
+++ polarity/user_commands.py
@@ -50,7 +50,6 @@ class Commands(Base):
 
 
 @lightbulb.add_checks(lightbulb.checks.has_roles(cfg.admin_role))
-@lightbulb.app_command_permissions(hikari.Permissions.USE_APPLICATION_COMMANDS)
 @lightbulb.option("response", "Response to post when this command is used", type=str)
 @lightbulb.option(
     "description", "Description of what the command posts or does", type=str
@@ -97,7 +96,6 @@ async def add_command(ctx: lightbulb.Context) -> None:
 
 
 @lightbulb.add_checks(lightbulb.checks.has_roles(cfg.admin_role))
-@lightbulb.app_command_permissions(hikari.Permissions.USE_APPLICATION_COMMANDS)
 @lightbulb.option(
     "name",
     "Name of the command to delete",
@@ -133,7 +131,6 @@ async def del_command(ctx: lightbulb.Context) -> None:
 
 
 @lightbulb.add_checks(lightbulb.checks.has_roles(cfg.admin_role))
-@lightbulb.app_command_permissions(hikari.Permissions.USE_APPLICATION_COMMANDS)
 @lightbulb.option(
     "new_description",
     "Description of the command to edit",
