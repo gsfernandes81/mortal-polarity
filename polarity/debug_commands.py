db0752365de6568b74360ab60f1d2049e5ffc1c2
gsfernandes81 <gsfernandes81@gmail.com>
2022-08-21 20:50:02 +0100

    Added weekly reset and alert debug commands

--- polarity/debug_commands.py
+++ polarity/debug_commands.py
@@ -16,6 +16,7 @@
 import lightbulb
 
 from . import autopost, cfg
+from .utils import _discord_alert
 
 
 @lightbulb.command(
@@ -30,6 +31,17 @@ async def daily_reset(ctx: lightbulb.Context) -> None:
 
 
 @lightbulb.command(
+    name="trigger_weekly_reset",
+    description="Sends a weekly reset signal",
+    guilds=(cfg.test_env,),
+)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def weekly_reset(ctx: lightbulb.Context) -> None:
+    ctx.bot.dispatch(autopost.WeeklyResetSignal(ctx.bot))
+    await ctx.respond("Weekly reset signal sent")
+
+
+@lightbulb.command(
     name="trigger_weekend_reset",
     description="Sends a weekend reset signal",
     guilds=(cfg.test_env,),
@@ -40,6 +52,33 @@ async def weekend_reset(ctx: lightbulb.Context) -> None:
     await ctx.respond("Weekend reset signal sent")
 
 
+@lightbulb.option(
+    name="text",
+    description="Text to alert with",
+    type=str,
+    default="Testing testing",
+)
+@lightbulb.command(
+    name="test_alert",
+    description="Sends a test alert",
+    guilds=(cfg.test_env,),
+)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def test_alert(ctx: lightbulb.Context) -> None:
+    await _discord_alert(
+        ctx.options.text,
+        bot=ctx.bot,
+        channel=cfg.alerts_channel_id,
+        mention_mods=True,
+    )
+    await ctx.respond("Done")
+
+
 def register_all(bot: lightbulb.BotApp) -> None:
-    for command in [daily_reset, weekend_reset]:
+    for command in [
+        daily_reset,
+        weekly_reset,
+        weekend_reset,
+        test_alert,
+    ]:
         bot.command(command)
