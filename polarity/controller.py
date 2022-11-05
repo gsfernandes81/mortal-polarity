d725b256bdbc106b9a680bf9022f1cf8f3999932
gsfernandes81 <gsfernandes81@gmail.com>
2022-11-05 15:24:13 +0000

    Add the kyber all_stop (shutdown) command

--- polarity/controller.py
+++ polarity/controller.py
@@ -13,23 +13,32 @@
 # You should have received a copy of the GNU Affero General Public License along with
 # mortal-polarity. If not, see <https://www.gnu.org/licenses/>.
 
-import lightbulb
+import lightbulb as lb
 
 from . import cfg
 
 
-@lightbulb.add_checks(lightbulb.checks.has_roles(cfg.admin_role))
-@lightbulb.command(
+@lb.add_checks(lb.checks.has_roles(cfg.admin_role))
+@lb.command(
     "kyber",
     "Commands for Kyber",
     guilds=[
         cfg.control_discord_server_id,
     ],
 )
-@lightbulb.implements(lightbulb.SlashCommandGroup)
+@lb.implements(lb.SlashCommandGroup)
 async def kyber():
     pass
 
 
-def register(bot: lightbulb.BotApp) -> None:
+@kyber.child
+@lb.add_checks(lb.checks.has_roles(cfg.admin_role))
+@lb.command("all_stop", "SHUT DOWN THE BOT", guilds=[cfg.control_discord_server_id])
+@lb.implements(lb.SlashSubCommand)
+async def all_stop(ctx: lb.Context):
+    await ctx.respond("Bot is going down now.")
+    await ctx.bot.close()
+
+
+def register(bot: lb.BotApp) -> None:
     bot.command(kyber)
