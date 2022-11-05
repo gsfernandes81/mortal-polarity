8fc8ac772d3e456dfe010a74e4c8e1af9d4e410d
gsfernandes81 <gsfernandes81@gmail.com>
2022-11-05 16:24:13 +0000

    Added a restart command for Kyber

--- polarity/controller.py
+++ polarity/controller.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU Affero General Public License along with
 # mortal-polarity. If not, see <https://www.gnu.org/licenses/>.
 
+import sys
+
 import lightbulb as lb
 
 from . import cfg
@@ -38,6 +40,17 @@ async def kyber():
 async def all_stop(ctx: lb.Context):
     await ctx.respond("Bot is going down now.")
     await ctx.bot.close()
+
+
+@kyber.child
+@lb.add_checks(lb.checks.has_roles(cfg.admin_role))
+@lb.command("restart", "RESTART THE BOT", guilds=[cfg.control_discord_server_id])
+@lb.implements(lb.SlashSubCommand)
+async def restart(ctx: lb.Context):
+    await ctx.respond("Bot is restarting now.")
+    # Exits with a non 0 code which is picked up by railway.app
+    # which restarts the bot
+    sys.exit(1)
 
 
 def register(bot: lb.BotApp) -> None:
