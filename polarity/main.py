c47fc1413c7403b94483c6aa9b5441fb710340db
gsfernandes81 <gsfernandes81@gmail.com>
2022-11-24 14:46:10 +0530

    Remove debug_commands.py

--- polarity/main.py
+++ polarity/main.py
@@ -20,7 +20,7 @@ import lightbulb as lb
 import uvloop
 from lightbulb.ext import tasks
 
-from . import cfg, controller, debug_commands, migration_commands, user_commands
+from . import cfg, controller, migration_commands, user_commands
 from .autopost import autoposts
 from .ls import lost_sectors
 from .weekly_reset import weekly_reset
@@ -55,7 +55,5 @@ if __name__ == "__main__":
     weekly_reset.register(bot)
     xur.register(bot)
     tasks.load(bot)
-    if cfg.test_env:
-        debug_commands.register_all(bot)
     migration_commands.register_all(bot)
     bot.run()
