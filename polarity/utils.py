7653d7cc5d5efedb2daecd0f70d8a9ff7624e2b7
gsfernandes81 <gsfernandes81@gmail.com>
2023-05-02 13:32:33 +0530

    Added retry loop to follow_link_single_step

--- polarity/utils.py
+++ polarity/utils.py
@@ -112,17 +112,26 @@ async def follow_link_single_step(
     url: str, logger=logging.getLogger("main/" + __name__)
 ) -> str:
     async with aiohttp.ClientSession() as session:
-        async with session.get(url, allow_redirects=False) as resp:
-            try:
-                return resp.headers["Location"]
-            except KeyError:
-                # If we can't find the location key, warn and return the
-                # provided url itself
-                logger.info(
-                    "Could not find redirect for url "
-                    + "{}, returning as is".format(url)
-                )
-                return url
+        retries = 10
+        retry_delay = 10
+        for i in range(retries):
+            async with session.get(url, allow_redirects=False) as resp:
+                try:
+                    return resp.headers["Location"]
+                except KeyError:
+                    # If we can't find the location key, warn and return the
+                    # provided url itself
+                    if resp.status >= 400:
+                        logger.error(
+                            "Could not find redirect for url "
+                            + "{}, (status {})".format(url, resp.status)
+                        )
+                        if i < retries - 1:
+                            logger.error("Retrying...")
+                        await asyncio.sleep(retry_delay)
+                        continue
+                    else:
+                        return url
 
 
 def _embed_for_migration(original_embed: h.Embed):
