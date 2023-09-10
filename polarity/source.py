7ebd7824301e3fcecea43599c917b5d5b7f93c76
gsfernandes81 <gsfernandes81@gmail.com>
2023-09-10 17:45:34 +0100

    Add source_code notice to source_code.py

--- polarity/source.py
+++ polarity/source.py
@@ -1,3 +1,18 @@
+# Copyright © 2019-present gsfernandes81
+
+# This file is part of "mortal-polarity".
+
+# mortal-polarity is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+
+# "mortal-polarity" is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License along with
+# mortal-polarity. If not, see <https://www.gnu.org/licenses/>.
+
 import lightbulb as lb
 
 SOURCE_CODE_NOTICE = """
