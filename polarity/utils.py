6a1475a9f50196fa0b1b35bb9098dc46923d6fbe
gsfernandes81 <gsfernandes81@gmail.com>
2023-11-10 09:56:32 +0530

    Remove unused regex

--- polarity/utils.py
+++ polarity/utils.py
@@ -33,10 +33,6 @@ from pytz import utc
 
 from . import cfg
 
-url_regex = re.compile(
-    "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-)
-
 
 class FeatureDisabledError(Exception):
     pass
